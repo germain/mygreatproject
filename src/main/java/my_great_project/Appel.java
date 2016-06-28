d4c99d62004b78536e7a53109afdcd5bf88efeca
Germain <seulgege@hotmail.fr>
2016-06-28 14:34:42 +0200

    les fichiers qu'on souhaite partager avec d'autres collaborateurs

---
+++ src/main/java/my_great_project/Appel.java
@@ -0,0 +1,18 @@
+package my_great_project;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.base.Strings;
+
+public class Appel {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Strings.isNullOrEmpty("gege"));
+		System.out.println(StringUtils.isEmpty("dada"));
+	
+
+	}
+
+}
